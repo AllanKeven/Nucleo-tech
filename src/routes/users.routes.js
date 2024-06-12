833af0d3266d95cade63a06e3838fb3f66ca0756
Allan <alencarmesquitaallankeven@gmail.com>
2024-06-11 23:01:23 -0300

    Adicionando_curso

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,16 +3,11 @@ const { userController } = require('../controller/userController');
 
 const userRoutes = express.Router();
 
-userRoutes.get('/allusers', new userController().getUser);
-
+userRoutes.get('/allUsers', new userController().getUser);
 userRoutes.post('/login', new userController().loginUser);
-
 userRoutes.post('/register', new userController().registerUser);
-
 userRoutes.patch('/update/:id', new userController().updateUser);
-
 userRoutes.delete('/remove/:id', new userController().deleteUser);
 userRoutes.post('/addProgressCourse/:userId', new userController().addCourseProgressInUserProfile);
-
 
 module.exports = userRoutes;
